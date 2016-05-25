b3f4335aa2928fa99e1797e7e44d804cf46e4027
Alessandro Arnone <arnone.alessandro@gmail.com>
2016-05-25 21:44:11 +0100

    Collect and pack responses

--- split/responses_packer.go
+++ split/responses_packer.go
@@ -1,10 +1,31 @@
 package split
 
-import "net/http"
+import (
+	"bytes"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+)
 
 // WriteResponses serialize the responses passed as argument into the ResponseWriter
-func WriteResponses(w http.ResponseWriter, esponses []*http.Response) error {
+func WriteResponses(w http.ResponseWriter, responses []*http.Response) error {
+	var buf bytes.Buffer
+	multipartWriter := multipart.NewWriter(&buf)
+
+	mimeHeaders := textproto.MIMEHeader(make(map[string][]string))
+	mimeHeaders.Set("Content-Type", "application/http")
+
+	for _, resp := range responses {
+		part, err := multipartWriter.CreatePart(mimeHeaders)
+		if err != nil {
+			return err
+		}
+		resp.Write(part)
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Not impelmented yet"))
+	w.Header().Set("Content-Type", mime.FormatMediaType("multipart/mixed", map[string]string{"boundary": multipartWriter.Boundary()}))
+	buf.WriteTo(w)
 	return nil
 }
