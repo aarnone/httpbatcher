ce1adc6d1835bdff792c012da9fd33056adcbe88
Alessandro Arnone <arnone.alessandro@gmail.com>
2016-06-08 23:58:00 +0100

    Implement UnpackResponse function

--- client.go
+++ client.go
@@ -1,13 +1,22 @@
 package httpbatcher
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"mime"
 	"mime/multipart"
 	"net/http"
 	"net/textproto"
 )
+
+// ErrInvalidMediaType is returned when the media type is not multipart/mixed
+var ErrInvalidMediaType = fmt.Errorf("Expected media type multipart/mixed")
+
+// ErrMultipartBoundaryNotDefined is returned when the media type is not multipart/mixed
+var ErrMultipartBoundaryNotDefined = fmt.Errorf("Media type multipart/mixed require boundary parameter")
 
 // BuildRequest encode a slice of requests into a single, multipart one
 func BuildRequest(requests []*http.Request, targetURL string) (*http.Request, error) {
@@ -36,4 +45,44 @@ func BuildRequest(requests []*http.Request, targetURL string) (*http.Request, er
 	req.Header.Set("Content-Type", mediaType)
 
 	return req, err
+}
+
+// UnpackResponse decode a multipart response into a slice containing its parts
+func UnpackResponse(fatResponse *http.Response) ([]*http.Response, error) {
+	if fatResponse.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(fatResponse.Body)
+		return nil, fmt.Errorf("Response status is %v: %v", fatResponse.StatusCode, string(body))
+	}
+
+	mediaType, params, err := mime.ParseMediaType(fatResponse.Header.Get("Content-Type"))
+	if err != nil {
+		return nil, fmt.Errorf("Can't parse the content type: %v", err)
+	}
+	if mediaType != "multipart/mixed" {
+		return nil, ErrInvalidMediaType
+	}
+	if params["boundary"] == "" {
+		return nil, ErrMultipartBoundaryNotDefined
+	}
+
+	mr := multipart.NewReader(fatResponse.Body, params["boundary"])
+	var responses []*http.Response
+	for {
+		np, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err := http.ReadResponse(bufio.NewReader(np), nil)
+		if err != nil {
+			return nil, fmt.Errorf("Unexpected error: %v", err)
+		}
+
+		responses = append(responses, resp)
+	}
+
+	return responses, nil
 }
--- client_test.go
+++ client_test.go
@@ -2,11 +2,13 @@ package httpbatcher
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"mime"
 	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"strings"
 	"testing"
 
@@ -138,4 +140,163 @@ func someRequests(n int) []*http.Request {
 	}
 
 	return requests
+}
+
+func TestUnpackResponseFailContentTypeCannotBeParsed(t *testing.T) {
+	// given a response with wrong content type
+	fatResponse := aFatResponse(aResponse(""))
+	fatResponse.Header.Set("Content-Type", "")
+
+	// when
+	responses, err := UnpackResponse(fatResponse)
+
+	// then error is not nil
+	assert.EqualError(t, err, "Can't parse the content type: mime: no media type")
+	assert.Empty(t, responses)
+}
+
+func TestUnpackResponseFailIfNotTheRightContentType(t *testing.T) {
+	// given a response with wrong content type
+	fatResponse := aFatResponse(aResponse(""))
+	fatResponse.Header.Set("Content-Type", "text/plain")
+
+	// when
+	responses, err := UnpackResponse(fatResponse)
+
+	// then error is not nil
+	assert.EqualError(t, err, ErrInvalidMediaType.Error())
+	assert.Empty(t, responses)
+}
+
+func TestUnpackResponseFailIfContentTypeDoNotIncludeABoundary(t *testing.T) {
+	// given a response with no boundary
+	fatResponse := aFatResponse(aResponse(""))
+	fatResponse.Header.Set("Content-Type", "multipart/mixed")
+
+	// when
+	responses, err := UnpackResponse(fatResponse)
+
+	// then error is not nil
+	assert.EqualError(t, err, ErrMultipartBoundaryNotDefined.Error())
+	assert.Empty(t, responses)
+}
+
+func TestUnpackResponseFailIfStatusNotOk(t *testing.T) {
+	// given a failing response
+	fatResponse := aResponse("really bad error")
+	fatResponse.Status = "400 Bad Request"
+	fatResponse.StatusCode = 400
+
+	// when
+	responses, err := UnpackResponse(fatResponse)
+
+	// then error report the http message
+	assert.EqualError(t, err, "Response status is 400: really bad error")
+	assert.Empty(t, responses)
+}
+
+func TestUnpackResponseShouldNotFailOnAValidResponse(t *testing.T) {
+	// given a valid response
+	fatResponse := aFatResponse(aResponse("valid response"))
+
+	// when
+	responses, err := UnpackResponse(fatResponse)
+
+	// then
+	assert.Nil(t, err)
+	assert.NotEmpty(t, responses)
+}
+
+func TestUnpackResponseShouldReturnTheResponsesInOrder(t *testing.T) {
+	// given a valid response with two parts
+	first, second := aResponse("first"), aResponse("second")
+	fatResponse := aFatResponse(first, second)
+
+	// when
+	responses, _ := UnpackResponse(fatResponse)
+
+	// then
+	firstBody, _ := ioutil.ReadAll(responses[0].Body)
+	assert.Equal(t, "first", string(firstBody))
+
+	secondBody, _ := ioutil.ReadAll(responses[1].Body)
+	assert.Equal(t, "second", string(secondBody))
+}
+
+func TestUnpackResponseShouldPreserveTheStatus(t *testing.T) {
+	// given a valid response with two parts with different status codes
+	first, second := aResponse("first"), aResponse("second")
+	first.StatusCode = 404
+	first.Status = "404 Not Found"
+
+	fatResponse := aFatResponse(first, second)
+
+	// when
+	responses, _ := UnpackResponse(fatResponse)
+
+	// then
+	assert.Equal(t, 404, responses[0].StatusCode)
+	assert.Equal(t, "404 Not Found", responses[0].Status)
+
+	assert.Equal(t, 200, responses[1].StatusCode)
+	assert.Equal(t, "200 OK", responses[1].Status)
+}
+
+func TestUnpackResponseShouldPreserveTheHeaders(t *testing.T) {
+	// given a valid response with two parts with custom header
+	first, second := aResponse("first"), aResponse("second")
+	first.Header.Set("X-First", "1st")
+	second.Header.Set("X-Second", "2nd")
+
+	fatResponse := aFatResponse(first, second)
+
+	// when
+	responses, _ := UnpackResponse(fatResponse)
+
+	// then
+	assert.Equal(t, "1st", responses[0].Header.Get("X-First"))
+	assert.Equal(t, "2nd", responses[1].Header.Get("X-Second"))
+}
+
+func aResponse(body string) *http.Response {
+	resp := new(http.Response)
+	resp.StatusCode = 200
+	resp.Status = "200 OK"
+	resp.Proto = "HTTP/1.0"
+	resp.ProtoMajor = 1
+	resp.ProtoMinor = 0
+	if body != "" {
+		resp.Body = ioutil.NopCloser(bytes.NewBufferString(body))
+		resp.ContentLength = int64(len(body))
+	}
+	resp.Header = http.Header{}
+	resp.Header.Set("Content-Type", "text/plain")
+	return resp
+}
+
+func aFatResponse(responses ...*http.Response) *http.Response {
+	var buf bytes.Buffer
+	multipartWriter := multipart.NewWriter(&buf)
+
+	mimeHeaders := textproto.MIMEHeader(make(map[string][]string))
+	mimeHeaders.Set("Content-Type", "application/http")
+
+	for _, resp := range responses {
+		part, _ := multipartWriter.CreatePart(mimeHeaders)
+		resp.Write(part)
+	}
+	multipartWriter.Close()
+
+	resp := new(http.Response)
+	resp.StatusCode = 200
+	resp.Status = "200 OK"
+	resp.Proto = "HTTP/1.0"
+	resp.ProtoMajor = 1
+	resp.ProtoMinor = 0
+	resp.Body = ioutil.NopCloser(&buf)
+	resp.ContentLength = int64(buf.Len())
+	resp.Header = http.Header{}
+	resp.Header.Set("Content-Type", mime.FormatMediaType("multipart/mixed", map[string]string{"boundary": multipartWriter.Boundary()}))
+
+	return resp
 }
