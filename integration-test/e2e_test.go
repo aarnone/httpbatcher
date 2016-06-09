2181f650798cf56952d7e1adb7882ebec6cdc032
Alessandro Arnone <arnone.alessandro@gmail.com>
2016-06-09 20:28:24 +0100

    Use the httpbatcher client to build the e2e test request

--- integration-test/e2e_test.go
+++ integration-test/e2e_test.go
@@ -3,16 +3,14 @@
 package test
 
 import (
-	"bufio"
-	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 	"strings"
 	"testing"
-	"time"
 
+	"github.com/aarnone/httpbatcher"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -32,58 +30,43 @@ func init() {
 }
 
 func TestMixedCalls(t *testing.T) {
-	requestBody := []byte(`POST /batch HTTP/1.1
-Host: ` + dockerHostname + `:8080
-Content-Length: 469
-Content-Type: multipart/mixed; boundary="===============7330845974216740156=="
-
-
---===============7330845974216740156==
-Content-Type: application/http
-Content-Transfer-Encoding: binary
-Content-ID: <b29c5de2-0db4-490b-b421-6a51b598bd22+1>
-
-GET /some/thing HTTP/1.1
-Host: serverA:8080
-Accept: */*
-
-
---===============7330845974216740156==
-Content-Type: application/http
-Content-Transfer-Encoding: binary
-Content-ID: <b29c5de2-0db4-490b-b421-6a51b598bd22+2>
-
-POST http://serverB:8080/weeee HTTP/1.1
-Accept: */*
-
-
---===============7330845974216740156==--`)
-
-	t.Log(string(requestBody))
-
-	conn, err := net.DialTimeout("tcp", dockerHostname+":8080", 5*time.Second)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-
-	_, err = conn.Write(requestBody)
+	r1, err := http.NewRequest(http.MethodGet, "http://serverA:8080/some/thing", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	resp, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	r2, err := http.NewRequest(http.MethodPost, "http://serverB:8080/weeee", strings.NewReader("some body"))
 	if err != nil {
 		panic(err)
 	}
 
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	fatRequest, err := httpbatcher.BuildRequest([]*http.Request{r1, r2}, "http://"+dockerHostname+":8080/batch")
+	if err != nil {
+		panic(err)
+	}
 
-	logResponse(t, resp)
+	logHttp(t, fatRequest)
+
+	fatResponse, err := http.DefaultClient.Do(fatRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Equal(t, http.StatusOK, fatResponse.StatusCode)
+
+	logHttp(t, fatResponse)
 }
 
-func logResponse(t *testing.T, resp *http.Response) {
-	dump, err := httputil.DumpResponse(resp, true)
+func logHttp(t *testing.T, r interface{}) {
+	var dump []byte
+	var err error
+	switch rt := r.(type) {
+	case *http.Response:
+		dump, err = httputil.DumpResponse(rt, true)
+	case *http.Request:
+		dump, err = httputil.DumpRequestOut(rt, true)
+	}
+
 	if err != nil {
 		t.Log(err)
 		return
